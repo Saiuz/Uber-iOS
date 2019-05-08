cef12f28e3470f41c2668b2e129e1c6029943882
Eduardo <eduardomarques@outlook.com>
2019-05-08 13:41:37 -0300

    :rocket: Exibição da localização do motorista e do usuário para o usuário, após o motorista aceitar a corrida

--- Uber/Controller/PassageiroViewController.swift
+++ Uber/Controller/PassageiroViewController.swift
@@ -16,7 +16,9 @@ class PassageiroViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var botaoChamarUber: UIButton!
     var gerenciadorLocalizacao = CLLocationManager()
     var localUsuario = CLLocationCoordinate2D()
+    var localMotorista = CLLocationCoordinate2D()
     var uberChamado = false
+    var uberACaminho = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,12 +38,66 @@ class PassageiroViewController: UIViewController, CLLocationManagerDelegate {
             // Faz uma query no database ordenando a pesquisa por e-mail e em seguida filtrando pelo e-mail do usuário logado
             let consultaRequisicoes = requisicoes.queryOrdered(byChild: "email").queryEqual(toValue: emailUsuario)
             
+            // Adiciona listener para quando o usuário chamar o Uber
             consultaRequisicoes.observe(.childAdded) { (snapshot) in
                 if snapshot.value != nil {
                     self.configBotaoCancelarUber()
                 }
             }
+            
+            // Adiciona listener para quando algum motorista aceitar a corrida
+            consultaRequisicoes.observe(.childChanged) { (snapshot) in
+                if let dados = snapshot.value as? [String: Any] {
+                    if let latMotorista = dados["motoristaLatitude"] {
+                        if let lonMotorista = dados["motoristaLongitude"] {
+                            // Cria a localização do motorista
+                            self.localMotorista = CLLocationCoordinate2D(latitude: latMotorista as! CLLocationDegrees, longitude: lonMotorista as! CLLocationDegrees)
+                            
+                            self.exibirMotoristaPassageiro()
+                        }
+                    }
+                }
+            }
         }
+    }
+    
+    func exibirMotoristaPassageiro() {
+        self.uberACaminho = true
+        
+        // Instancia distancias para motorista e passageiros para realizar o cálculo de distancia
+        let motoristaLocation = CLLocation(latitude: self.localMotorista.latitude, longitude: self.localMotorista.longitude)
+        let passageiroLocation = CLLocation(latitude: self.localUsuario.latitude, longitude: self.localUsuario.longitude)
+        
+        // Calcular distância entre motorista e passageiro
+        let distancia = motoristaLocation.distance(from: passageiroLocation)
+        let distanciaFinal = round((distancia/1000)) // Distancia / 1000 = distancia em KM | round arredonda o valor
+        
+        // Alterações no botão de chamar Uber
+        self.botaoChamarUber.backgroundColor = UIColor(displayP3Red: 0.0, green: 0.800, blue: 0.0, alpha: 1)
+        self.botaoChamarUber.setTitle("Motorista \(distanciaFinal) KM distante", for: .normal)
+        
+        // Exibir diretamente o passageiro e motorista no mapa
+        mapa.removeAnnotations(mapa.annotations)
+        
+        // Calcula um raio de distancia entre usuário e motorista para configurar no mapa uma distancia que apresente as duas localizações na tela
+        let latDiferenca = abs(self.localUsuario.latitude - self.localMotorista.latitude) * 300000 // abs = valor absoluto
+        let lonDiferenca = abs(self.localUsuario.longitude - self.localMotorista.longitude) * 300000
+        
+        // Cria a região para exibir no mapa
+        let regiao = MKCoordinateRegion(center: self.localUsuario, latitudinalMeters: latDiferenca, longitudinalMeters: lonDiferenca)
+        mapa.setRegion(regiao, animated: true)
+        
+        // Anotacao motorista
+        let anotacaoMotorista = MKPointAnnotation()
+        anotacaoMotorista.coordinate = self.localMotorista
+        anotacaoMotorista.title = "Motorista"
+        mapa.addAnnotation(anotacaoMotorista)
+        
+        // Anotacao passageiro
+        let anotacaoPassageiro = MKPointAnnotation()
+        anotacaoPassageiro.coordinate = self.localUsuario
+        anotacaoPassageiro.title = "Sua localização"
+        mapa.addAnnotation(anotacaoPassageiro)
     }
     
     // Método utilizado para atualizar a localização do usuário
@@ -51,17 +107,21 @@ class PassageiroViewController: UIViewController, CLLocationManagerDelegate {
             // Configura local atual do usuário
             self.localUsuario = coordenadas
             
-            let regiao = MKCoordinateRegion(center: coordenadas, latitudinalMeters: 200, longitudinalMeters: 200)
-            mapa.setRegion(regiao, animated: true)
-            
-            // Remove anotações antes de criar uma nova, para evitar que apareça várias anotações a cada atualização de localização
-            mapa.removeAnnotations(mapa.annotations)
-            
-            //Cria uma anotação (marcador) para o local do usuário
-            let anotacaoUsuario = MKPointAnnotation()
-            anotacaoUsuario.coordinate = coordenadas
-            anotacaoUsuario.title = "Sua localização"
-            mapa.addAnnotation(anotacaoUsuario) // Adiciona o marcador no mapa
+            if self.uberACaminho {
+                self.exibirMotoristaPassageiro()
+            } else {
+                let regiao = MKCoordinateRegion(center: coordenadas, latitudinalMeters: 200, longitudinalMeters: 200)
+                mapa.setRegion(regiao, animated: true)
+                
+                // Remove anotações antes de criar uma nova, para evitar que apareça várias anotações a cada atualização de localização
+                mapa.removeAnnotations(mapa.annotations)
+                
+                //Cria uma anotação (marcador) para o local do usuário
+                let anotacaoUsuario = MKPointAnnotation()
+                anotacaoUsuario.coordinate = coordenadas
+                anotacaoUsuario.title = "Sua localização"
+                mapa.addAnnotation(anotacaoUsuario) // Adiciona o marcador no mapa
+            }
         }
     }
     
