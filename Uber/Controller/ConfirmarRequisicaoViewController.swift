0de06b9f13cd49320f20e5a5539c7e8c2e92be8b
Eduardo <eduardomarques@outlook.com>
2019-05-09 17:38:28 -0300

    :rocket: Alteração do status da corrida para o Motorista

--- Uber/Controller/ConfirmarRequisicaoViewController.swift
+++ Uber/Controller/ConfirmarRequisicaoViewController.swift
@@ -11,13 +11,21 @@ import MapKit
 import FirebaseDatabase
 import FirebaseAuth
 
+// Enum = Grupo de constantes
+enum StatusCorrida: String {
+    case EmRequisicao, PegarPassageiro, IniciarViagem, EmViagem
+}
+
 class ConfirmarRequisicaoViewController: UIViewController {
     @IBOutlet weak var mapa: MKMapView!
+    @IBOutlet weak var botaAaceitarCorrida: UIButton!
     
     var nomePassageiro = ""
     var emailPassageiro = ""
     var localPassageiro = CLLocationCoordinate2D()
     var localMotorista = CLLocationCoordinate2D()
+    
+    var status: StatusCorrida = .EmRequisicao // Cria uma var do tipo do enum
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,36 +44,57 @@ class ConfirmarRequisicaoViewController: UIViewController {
     }
     
     @IBAction func aceitarCorrida(_ sender: Any) {
-        // Atualizar requisição no database
-        let requisicoes = Database.database().reference().child("requisicoes")
-        
-        if let emailMotorista = Auth.auth().currentUser?.email {
-            requisicoes.queryOrdered(byChild: "email").queryEqual(toValue: self.emailPassageiro).observeSingleEvent(of: .childAdded) { (snapshot) in
-                let dadosMotorista = [
-                    "motoristaEmail" : emailMotorista,
-                    "motoristaLatitude" : self.localMotorista.latitude,
-                    "motoristaLongitude" : self.localMotorista.longitude
-                    ] as [String : Any]
-                snapshot.ref.updateChildValues(dadosMotorista)
-            }
-        }
-        
-        // Exibir o caminho até o passageiro no mapa
-        let passageiroCLL = CLLocation(latitude: localPassageiro.latitude, longitude: localPassageiro.longitude)
-        CLGeocoder().reverseGeocodeLocation(passageiroCLL) { (local, erro) in
-            if erro == nil {
-                if let dadosLocal = local?.first {
-                    let placeMark = MKPlacemark(placemark: dadosLocal)
+        if self.status == StatusCorrida.EmRequisicao {
+            // Atualizar requisição no database
+            let requisicoes = Database.database().reference().child("requisicoes")
+            
+            if let emailMotorista = Auth.auth().currentUser?.email {
+                requisicoes.queryOrdered(byChild: "email").queryEqual(toValue: self.emailPassageiro).observeSingleEvent(of: .childAdded) { (snapshot) in
+                    let dadosMotorista = [
+                        "motoristaEmail" : emailMotorista,
+                        "motoristaLatitude" : self.localMotorista.latitude,
+                        "motoristaLongitude" : self.localMotorista.longitude,
+                        "status" : StatusCorrida.PegarPassageiro.rawValue // rawValue converte o enum em String
+                        ] as [String : Any]
                     
-                    let mapaItem = MKMapItem(placemark: placeMark)
-                    mapaItem.name = self.nomePassageiro
-                    
-                    // Modo como a pessoa se deslocará até o local (andando, dirigindo, etc)
-                    let opcoes = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
-                    // Abre o recurso de rotas dentro do mapa
-                    mapaItem.openInMaps(launchOptions: opcoes)
+                    snapshot.ref.updateChildValues(dadosMotorista)
+                    self.pegarPassageiro()
                 }
             }
+            
+            // Exibir o caminho até o passageiro no mapa
+            let passageiroCLL = CLLocation(latitude: localPassageiro.latitude, longitude: localPassageiro.longitude)
+            CLGeocoder().reverseGeocodeLocation(passageiroCLL) { (local, erro) in
+                if erro == nil {
+                    if let dadosLocal = local?.first {
+                        let placeMark = MKPlacemark(placemark: dadosLocal)
+                        
+                        let mapaItem = MKMapItem(placemark: placeMark)
+                        mapaItem.name = self.nomePassageiro
+                        
+                        // Modo como a pessoa se deslocará até o local (andando, dirigindo, etc)
+                        let opcoes = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+                        // Abre o recurso de rotas dentro do mapa
+                        mapaItem.openInMaps(launchOptions: opcoes)
+                    }
+                }
+            }
+        } else if true {
+            
         }
+    }
+    
+    func pegarPassageiro() {
+        // Altera o status do Passageiro
+        self.status = .PegarPassageiro
+        
+        // Alterna o botão
+        self.configBotaoPegarPassageiro()
+    }
+    
+    func configBotaoPegarPassageiro() {
+        self.botaAaceitarCorrida.setTitle("A caminho do passageiro", for: .normal)
+        self.botaAaceitarCorrida.isEnabled = false
+        self.botaAaceitarCorrida.backgroundColor = UIColor(displayP3Red: 0.502, green: 0.502, blue: 0.502, alpha: 1)
     }
 }
